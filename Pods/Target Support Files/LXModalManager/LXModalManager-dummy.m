de1c27cfe3cd5a1112a17044c73d5e45051f6dda
lixiang <lixiang@gokuaidian.com>
2020-04-29 23:34:03 +0800

    修改tag

---
+++ Pods/Target Support Files/LXModalManager/LXModalManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LXModalManager : NSObject
+@end
+@implementation PodsDummy_LXModalManager
+@end
--- Pods/Target Support Files/Pods-LXModalModule/Pods-LXModalModule-frameworks.sh
+++ Pods/Target Support Files/Pods-LXModalModule/Pods-LXModalModule-frameworks.sh
@@ -198,9 +198,11 @@ fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/LXFitManager/LXFitManager.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/LXModalManager/LXModalManager.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/LXFitManager/LXFitManager.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/LXModalManager/LXModalManager.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
